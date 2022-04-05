30744d1d5c1420f89656df7be5253864002f5084
IlyaMinchenko <sir.over@yandex.ru>
2022-04-05 12:05:00 +0500

    Stable version

--- Index.js
+++ Index.js
@@ -1,11 +1,9 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  createBottomTabNavigator
-} from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCoordinate, fetchCoords } from "./store/coordinateSlice";
-import { set} from "./store/dataSlice";
+import { set } from "./store/dataSlice";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { setAir } from "./store/airSlice";
 import TableComponent from "./TableComponent";
@@ -14,16 +12,15 @@ import Settings from "./Settings";
 import locate from "./locate";
 import Dalnost from "./Dalnost";
 import Azimut from "./Azimut";
-import { getMultiple} from "./localStorage";
+import { getMultiple } from "./localStorage";
 
 export default function Index() {
   const Tab = createBottomTabNavigator();
   const dispatch = useDispatch();
   const coordinate = useSelector(selectCoordinate);
   useEffect(() => {
-    dispatch(fetchCoords())
+    dispatch(fetchCoords());
   }, [dispatch]);
-
 
   useEffect(() => {
     getMultiple().then((res) => {
@@ -33,36 +30,32 @@ export default function Index() {
 
   //extraReducer запроса и вывода координат
 
-
   useEffect(() => {
-    let timerId
+    let timerId;
     //запрос данных с FlightRadar24 по введенным координатам
-    timerId = setTimeout(
-      function work(){
-      let result;
+    timerId = setTimeout(function work() {
+      // console.log(coordinate)
       getMultiple().then((res) => {
         const lat = +res[0][1][1];
         const lon = +res[0][1][2];
         const latPred = +res[0][1][4];
         const lonPred = +res[0][1][5].substring(0, res[0][1][5].length - 1);
-      locate(lat,lon,latPred,lonPred)
-      .then((res) => 
-        result = res)
-
-      //добавление в массив результатов дальности до самолета
-      .then((res) =>
-        result.map((item) => {
-          item.push(Dalnost(item, coordinate.lat, coordinate.lon));
-          item.push(Azimut(item, coordinate.lat, coordinate.lon));
-        })
-      )
-      //добавление в массив результатов азимута самолета
-      .then((res) => dispatch(set(result)))
-      .then((res)=>
-      timerId = setTimeout(work, 1000))
-    })}, 1000)
-  }, 
-[coordinate]);
+        locate(lat, lon, latPred, lonPred)
+          .then((res) => {
+            res.map((item) => {
+              item.push(Dalnost(item, lat, lon));
+              item.push(Azimut(item, lat, lon));
+            });
+            return res;
+          })
+          //добавление в массив результатов азимута самолета
+          .then((res) => {
+            dispatch(set(res));
+          })
+          .then((res) => (timerId = setTimeout(work, 1000)));
+      });
+    }, 1000);
+  }, [coordinate]);
 
   return (
     <NavigationContainer>
@@ -83,10 +76,7 @@ export default function Index() {
           tabBarInactiveTintColor: "gray",
         })}
       >
-        <Tab.Screen
-          name="Карта"
-          component={Map}
-        />
+        <Tab.Screen name="Карта" component={Map} />
         <Tab.Screen name="Таблица" component={TableComponent} />
         <Tab.Screen name="Настройки" component={Settings} />
       </Tab.Navigator>
